5a3173abc4ec14d804ab914172c02d941f9461c9
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2018-04-17 15:45:52 +0200

    added option for sharepoint url to .env

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 
-export const sharepointUrl = 'https://abb.sharepoint.com/sites/CombiX/LabInventory';
+export const sharepointUrl = process.env.SHAREPOINT_URL || 'https://abb.sharepoint.com/sites/CombiX/LabInventory';
 
 export const tokenSecret = process.env.TOKEN_SECRET;
