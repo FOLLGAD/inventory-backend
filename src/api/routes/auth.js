353aee6bb89d210aeaa3c68fc6aafa5624ef4232
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2018-04-26 14:25:42 +0200

    test route

--- src/api/routes/auth.js
+++ src/api/routes/auth.js
@@ -3,6 +3,8 @@ import { getToken } from '../middleware';
 import { basicToCredentials } from '../../utils';
 import { User } from '../models';
 
+import * as spauth from 'node-sp-auth';
+
 let router = new Router();
 
 router
@@ -28,12 +30,31 @@ router
                         res.status(200).json(Object.assign({}, d._doc, { token }));
                     } else {
                         User.create({ email: username });
-                        res.status(200).json(Object.assign({}, { token }));
+                        res.status(200).json({ token });
                     }
                 })
         }).catch(err => {
             res.status(401).send("Wrong credentials or failed to connect to auth service");
         });
+    })
+    .get('/test', (req, res) => {
+        let { username, password } = basicToCredentials(req.headers.authorization);
+
+        // authurl: https://abb.sharepoint.com/
+        // basic auth
+
+        spauth.getAuth(req.headers.authurl, {
+            username,
+            password,
+        })
+        .then(resp => {
+            console.log(resp)
+            res.sendStatus(200)
+        })
+        .catch(resp => {
+            console.log(resp)
+            res.sendStatus(400)
+        })
     })
 
 export default router;
